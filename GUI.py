59457e8c2c1b8a3de39ea40cb94c92310a206394
Simon <simon.ferre.eu@gmail.com>
2020-04-04 18:50:41 +0200

    Création Main window Organisation layout board

--- GUI.py
+++ GUI.py
@@ -10,12 +10,25 @@ import  client
 # non blocking subcriber
 # https://stackoverflow.com/questions/26012132/zero-mq-socket-recv-call-is-blocking
 
+class Artifice(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.content = Fenetre()
+        self.setCentralWidget(self.content)
+
+        menubar = self.menuBar()
+        menu = menubar.addMenu('File')
+        db_action = menu.addAction("Open file")
+        db_action.setStatusTip("Select a file to use as a database")
+        # db_action.triggered.connect(self.open_new_db)
+
+        self.statusBar().showMessage("Ready")
+
 class Fenetre(QWidget):
     def __init__(self):
         QWidget.__init__(self)
-        size = self.screen().size()
-        self.res_x = size.width()
-        self.res_y = size.height()
+        self.res_x = self.size().width()
+        self.res_y = self.size().height()
         self.resize(self.res_x, self.res_y)
         self.board = gm.Board()
         self.team = gm.Team()
@@ -39,12 +52,12 @@ class Fenetre(QWidget):
         self.layout_buts_play.addWidget(self.but_join)
         self.layout_buts_play.addWidget(self.but_launch)
 
-        # On affiche les tas de cartes
+        # On affiche le plateau
         self.wid_board = Widget_board()
         self.wid_board.add_board(self.board)
 
         # Ordre ['r','b','y','g','w']
-        self.tas_labels = {'r': QLabel("r0"), 'b': QLabel("b0"), 'y': QLabel("y0"), 'g': QLabel("g0"), 'w': QLabel("w0")}
+        # self.tas_labels = {'r': QLabel("r0"), 'b': QLabel("b0"), 'y': QLabel("y0"), 'g': QLabel("g0"), 'w': QLabel("w0")}
         # self.draw_game()
 
 
@@ -250,45 +263,68 @@ class Widget_board(QWidget):
         ratio_y = 0.66
         p = p.scaled(int(ratio_x*res_x), int(ratio_y*res_y), Qt.KeepAspectRatio)
         self.label.setPixmap(p)
+        self.board_x = p.width()
+        self.board_y = p.height()
         self.layout = QFormLayout()
         self.setLayout(self.layout)
         self.layout.addWidget(self.label)
 
-        # On affiche les tas a remplir
+        # On definit le layout principal
         self.layout_board = QVBoxLayout()
+
+        # On créé le layout pour les erreurs commises
+        self.layout_error = QHBoxLayout()
+        self.wid_error = QWidget()
+        self.wid_error.setLayout(self.layout_error)
+        self.layout_board.addWidget(self.wid_error)
+
+        # On affiche les tas a remplir
         self.label.setLayout(self.layout_board)
         self.layout_tas = QHBoxLayout()
         self.layout_tas.setSpacing(int(res_x*0.0095))
         self.layout_tas.setContentsMargins(int(res_x*0.0735), int(res_y*0.33), int(res_x*0.0735), int(res_y*0.01))
         self.wid_tas = QWidget()
         self.wid_tas.setLayout(self.layout_tas)
-        self.layout_board.addWidget(self.wid_tas,3)
+        self.layout_board.addWidget(self.wid_tas)
 
-        # On affiche les indices et les bombes erreurs
-        self.wid_clue_miss = QWidget()
-        self.wid_clue_miss.setFixedHeight(int(res_y*0.165))
+        # On affiche les indices
         self.layout_clue = QHBoxLayout()
-        self.wid_clue_miss.setLayout(self.layout_clue)
-        self.label_clue = QLabel("Indice :" + str(0))
-        self.label_miss = QLabel("Erreurs :" + str(0))
-        self.layout_clue.addWidget(self.label_clue)
-        self.layout_clue.addWidget(self.label_miss)
-        self.wid_pioche = QLabel("Cartes restantes : " + str(0))
-        self.wid_dism_stack = QCarte()
-        self.layout_clue.addWidget(self.wid_pioche)
-        self.layout_clue.addWidget(self.wid_dism_stack)
-        self.layout_board.addWidget(self.wid_clue_miss)
+        self.wid_clues = QWidget()
+        self.wid_clues.setLayout(self.layout_clue)
+        self.layout_board.addWidget(self.wid_clues)
+        self.layout_board.addWidget(self.wid_clues)
 
+        # On affiche la pioche, la carte jouee et la defausse
+        self.wid_draw_play_disc = QWidget()
+        self.layout_dpd = QHBoxLayout()
+        self.wid_draw_play_disc.setLayout(self.layout_dpd)
+        self.wid_pioche = QLabel("Pioche")
+        self.wid_play_card = QCarte()
+        self.wid_dism_stack = QCarte()
+        self.layout_dpd.addWidget(self.wid_pioche)
+        self.layout_dpd.addWidget(self.wid_play_card)
+        self.layout_dpd.addWidget(self.wid_dism_stack)
+        self.layout_board.addWidget(self.wid_draw_play_disc)
 
     def add_board(self, board):
         self.clear_board()
+        # On affiche le nombre d'erreurs
+        for error_i in range(board.miss):
+            lab_error = QLabel()
+            pix_error = QPixmap("images/token/token_error")
+            pix_error = pix_error.scaled(25, 25, Qt.KeepAspectRatio)
+            lab_error.setPixmap(pix_error)
+            self.layout_error.addWidget(lab_error)
+        self.layout_error.setSpacing(int(7/802*self.board_x))
+        print("Board " + str(self.board_x) + " " + str(self.board_y))
+        self.layout_error.setContentsMargins(int(0.412*self.board_x), int(0.029*self.board_y),
+                                                 int(0.412*self.board_x), int(0.0*self.board_y))
+        # On affiche les stacks
         for stack_key in board.stack_dic.keys():
             stack = board.stack_dic[stack_key]
             if len(stack.card_list) > 0:
                 print("joue 1")
                 carte_top = stack.card_list[-1]
-                # path = "images/" + carte_top.color + str(carte_top.value)
-                # wid_carte = QCarte(carte = carte_top)
                 path = "images/stack_" + carte_top.color + str(carte_top.value)
                 print(path)
                 wid_stack = QLabel()
@@ -302,14 +338,22 @@ class Widget_board(QWidget):
             else:
                 # path = "images/naught_" + stack_key
                 print("Pas de carte " + stack_key)
-                # wid_carte = QCarte()
-            # wid_carte.set_image(path)
-            # self.layout_tas.addWidget(wid_carte)
+                
         # On update les miss et les clues
-        self.label_clue = QLabel("Indices :" + str(board.clues))
-        self.label_miss = QLabel("Erreurs :" + str(board.miss))
-        self.layout_clue.addWidget(self.label_clue)
-        self.layout_clue.addWidget(self.label_miss)
+        # On affiche les indices
+        for i in range(board.clues):
+            lab_clue = QLabel()
+            pix_clue = QPixmap("images/token/token_clue")
+            pix_clue = pix_clue.scaled(25, 25, Qt.KeepAspectRatio)
+            lab_clue.setPixmap(pix_clue)
+            self.layout_clue.addWidget(lab_clue)
+        self.layout_clue.setSpacing(int(7/802*self.board_x))
+        self.layout_clue.setContentsMargins(int(0.3*self.board_x), int(0.01*self.board_y), int(0.3*self.board_x), int(0*self.board_y) )
+        # self.layout_clue.addWidget(self.label_miss)
+        # On affiche les piles de pioche, la carte jouee et la defausse
+        # TODO ajouter la carte jouee
+        self.wid_pioche = QLabel("Cartes restantes : " + str(len(board.draw_list.card_list)))
+        self.layout_dpd.addWidget(self.wid_pioche)
         if len(board.discard_list.card_list)>0:
             self.wid_dism_stack = QCarte(board.discard_list.card_list[-1])
         else:
@@ -317,15 +361,17 @@ class Widget_board(QWidget):
         # size = self.screen().size()
         # res_y = size.height()
         # self.wid_dism_stack.setFixedHeight(int(res_y*0.165))
-        self.layout_clue.addWidget(self.wid_dism_stack)
-        self.wid_pioche = QLabel("Cartes restantes : " + str(len(board.draw_list.card_list)))
-        self.layout_clue.addWidget(self.wid_pioche)
+        self.layout_dpd.addWidget(self.wid_dism_stack)
 
     def clear_board(self):
+        for i in reversed(range(self.layout_error.count())):
+            self.layout_error.itemAt(i).widget().setParent(None)
         for i in reversed(range(self.layout_tas.count())):
             self.layout_tas.itemAt(i).widget().setParent(None)
         for i in reversed(range(self.layout_clue.count())):
             self.layout_clue.itemAt(i).widget().setParent(None)
+        for i in reversed(range(self.layout_dpd.count())):
+            self.layout_dpd.itemAt(i).widget().setParent(None)
 
 
 app = QApplication.instance()
@@ -334,7 +380,8 @@ if not app:
 
 # screen_resolution = app.desktop().screenGeometry()
 # res_x, res_y = screen_resolution.width(), screen_resolution.height()
-fen = Fenetre()
-fen.show()
-
+# fen = Fenetre()
+# fen.show()
+artif = Artifice()
+artif.show()
 app.exec_()
